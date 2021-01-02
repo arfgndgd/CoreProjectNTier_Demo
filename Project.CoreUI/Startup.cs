dbcebd2e0a8505b6d8cd922fc1b05a51c59468a5
Arif Gündoğdu <gundogduarif4@gmail.com>
2021-01-02 18:21:22 +0300

    Kütüphaneler şimdilik tamamlandı.

--- Project.CoreUI/Startup.cs
+++ Project.CoreUI/Startup.cs
@@ -24,6 +24,48 @@ namespace Project.CoreUI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+
+            // Normal(N - Layered olmayan normal projelerde) Dependency Injection yapýsý Core'da su sekilde kurulur
+
+            //Core'un otomatik olarak hangi Interface'i nasýl algýlayacagýný belirten bir mimarisi vardýr...Bu sisteme sizin özellikle bir nesne göndermenize gerek yoktur bu otomatik yapýlýr. Ancak hangi Interface'in olacagýný belirtmelisiniz...
+
+            #region AddScoped
+            //services.AddScoped<IProductRepository,ProductRepository>().;
+
+            /*
+             public IActionResult Deneme(IProductRepository item , IProductRepository item2)
+            {
+            
+                return View();
+            }
+     
+             */
+
+            #endregion
+
+
+            //services.AddSingleton<IProductRepository,ProductRepository>().;
+
+
+            #region AddTransient
+            //services.AddTransient<IProductRepository,ProductRepository>(); 
+
+            //Her sýnýf tetikleniþi icin ayrý bir instance alýr
+
+            /*
+            public IActionResult Deneme(IProductRepository item,IProductRepository item2)
+            {
+
+            }
+             
+             */
+            #endregion
+
+            //Yukarýdaki demek istedigimiz þey proje bir yerde IProductRepository gördügünde ona nesne olarak ne göndermeli onu söylemektir...Dikkat ederseniz burada instance alma iþlemini bile siz yapmýyorsunuz... Bu instance alma iþlemi Dependency Injection'ýn Core'da otomatik olarak entegre edilmesiyle gercekleþiyor...AddSingleton ilgili nesne icin bir SingletonPattern görevi görürken,AddScoped bir HTTPRequest'i icin sadece bir nesne alma görevi görürken,AddTransient her class tetiklendiginde bir nesne yaratan bir Dependenjy Injection iþlemi yapar...
+
+            // Eger katmanlý bir yapý kuruyorsanýz bu AddTransient olayýný kendi mimarinize göre þekillendirmek zorundasýnýz...Bunun iki yöntemi vardýr...Ya Autofac kütüphanesi kullanarak Injection Extension yapmak (Yani Injection'ý geniþletmek) veya kendi Extension metodunuzu static sýnýfta yaratarak bu Injection Extension'i manual yapmak...
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
